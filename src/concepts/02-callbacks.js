f6429ee16c1cd0ae85ad8489d8ca66665aaf1a14
Jose Ledo <j.varela@sixtema.es>
2024-12-04 09:54:08 +0100

    done

---
+++ src/concepts/02-callbacks.js
@@ -0,0 +1,33 @@
+import {heroes} from '../data/heroes';
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const callbacksComponent = (element) => {
+
+    const id = '5d86371fd55e2e2a30fe1cc3';
+    findHero(id, (error, hero) => {
+        //
+        if (error){
+            element.innerHTML = error;
+            return;
+        }
+        element.innerHTML = hero.name;    
+    });
+    
+};
+
+/**
+ * 
+ * @param {String} id 
+ * @param {(error: String|Null, hero:Object) => void} callback 
+ */
+const findHero = (id, callback) => {
+    const hero = heroes.find(hero => hero.id ===id);
+    if (!hero) {
+        callback (`Hero with id ${id} not found.`);
+        return;
+    }
+
+    callback( null, hero );
+}
